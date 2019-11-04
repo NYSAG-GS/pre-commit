2e51e47780dfcc7110d9e06d91f87aabd6c896ed
NYSAG-GS <gautam.sisodia@ag.ny.gov>
2019-11-04 12:54:52 -0500

    updated pip-freeze-check to work in subdirectories

--- hooks/pip-freeze-check.sh
+++ hooks/pip-freeze-check.sh
@@ -3,9 +3,10 @@
 # from https://gist.github.com/milancermak/3849310#file-pre-commit-sh
 # make sure requirements.txt is up to date with every commit
 # by comparing the output of pip freeze
-pip freeze | diff requirements.txt - > /dev/null
+GIT_DIR=$(git rev-parse --show-toplevel)
+pip freeze | diff $GIT_DIR/requirements.txt - > /dev/null
 if [ $? != 0 ]
 then
-    echo "Missing python module dependencies in requirements.txt. Run 'pip freeze > requirements.txt' to update."
+    echo "Missing python module dependencies in requirements.txt. Run 'pip freeze > requirements.txt' in this repo's home directory to update."
     exit 1
 fi
